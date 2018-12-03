a51b7192797a9e28efadde66404b4c03755c99a7
Michael Donovan <mike@eggborne.com>
2018-12-03 13:50:32 -0800

    Add cardSize object and Card components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@ import style from '../css/styles.css';
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import Card from './Card';
 import createGame from '../scripts/init';
 
 let shorterDimension;
 window.innerWidth < window.innerHeight
   ? shorterDimension = window.innerWidth
   : shorterDimension = window.innerHeight;
+
+let cardSize = {
+  width: '3rem',
+  height: '5rem'
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -26,7 +32,24 @@ class App extends React.Component {
       <div id='container'>
         <Header />
         <div id='game'>
-          
+          <div className='hand'>
+            <Card size={cardSize}
+              value={4}   
+              type='house'
+            />
+            <Card size={cardSize}
+              value={2}   
+              type='plus'
+            />
+            <Card size={cardSize}
+              value={3}   
+              type='minus'
+            />
+            <Card size={cardSize}
+              value={10}   
+              type='plus'
+            />
+          </div>
         </div>
         <Footer />
       </div>
