b91e4b67fd356871d570c2f9e28a4e9880acb00d
Michael Donovan <mike@eggborne.com>
2018-12-03 22:50:30 -0800

    Add End Turn and Stand buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 function Footer(props) {
   return (
     <div id='footer'>
-      footer
+      <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
+      <button className='move-button' onClick={props.onClickStand} id='stand-button'>Stand</button>
     </div>
   );
 }
 
 Footer.propTypes = {
-
+  onClickEndTurn: PropTypes.func,
+  onClickStand: PropTypes.func
 };
 
 
