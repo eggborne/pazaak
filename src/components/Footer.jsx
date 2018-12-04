1068458d6749b1c03c2ba857fcac070118b09ecd
Michael Donovan <mike@eggborne.com>
2018-12-04 04:28:15 -0800

    Add prop to control visibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Footer(props) {
   return (
-    <div id='footer'>
+    <div style={props.display} id='footer'>
       <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
       <button className='move-button' onClick={props.onClickStand} id='stand-button'>Stand</button>
     </div>
@@ -12,7 +12,8 @@ function Footer(props) {
 
 Footer.propTypes = {
   onClickEndTurn: PropTypes.func,
-  onClickStand: PropTypes.func
+  onClickStand: PropTypes.func,
+  display: PropTypes.object
 };
 
 
