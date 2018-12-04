b357403ea34dde09b48fd14185b7c9a5d19051e3
Michael Donovan <mike@eggborne.com>
2018-12-03 22:51:52 -0800

    Add flash() function

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -2,3 +2,17 @@ export function pointWithinBounds(pointX, pointY, boundsX, boundsY, boundsWidth,
   return pointX > boundsX && pointX < boundsX + boundsWidth
     && pointY > boundsY && pointY < boundsY + boundsHeight;
 }
+
+export function flash(elementId, property, origColor, flashColor) {
+  let el = document.getElementById(elementId);
+  el.style[property] = flashColor;
+  setTimeout(() => {
+    el.style[property] = origColor;
+    setTimeout(() => {
+      el.style[property] = flashColor;
+      setTimeout(() => {
+        el.style[property] = origColor;
+      }, 50)
+    }, 50)
+  }, 50);
+}
