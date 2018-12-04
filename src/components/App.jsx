3d3d03a7373de94ac8d6ce6ddbb9c2dccefda857
Michael Donovan <mike@eggborne.com>
2018-12-03 19:08:13 -0800

    Create grids and player-area elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,32 +33,50 @@ class App extends React.Component {
       <div id='container'>
         <Header />
         <div id='game-board'>
+
           <div id='opponent-hand' className='hand'>
             <CardBack size={cardSize} />
             <CardBack size={cardSize} />
             <CardBack size={cardSize} />
             <CardBack size={cardSize} />
           </div>
-          <div id='player-hand' className='hand'>
-            <Card size={cardSize}
-              value={4}
-              type='house'
-            />
-            <Card size={cardSize}
-              value={2}
-              type='plus'
-            />
-            <Card size={cardSize}
-              value={3}
-              type='minus'
-            />
-            <Card size={cardSize}
-              value={10}
-              type='plus'
-            />
+
+          <div id='grids'>
+            <div id='opponent-area' className='player-area'>
+              <div className='deal-grid'>
+                <Card size={cardSize} value={2} type='house' />
+                <Card size={cardSize} value={5} type='house' />
+                <Card size={cardSize} value={8} type='house' />
+                <Card size={cardSize} value={6} type='house' />
+                <Card size={cardSize} value={7} type='house' />
+                <Card size={cardSize} value={10} type='house' />
+                <Card size={cardSize} value={3} type='house' />
+                <Card size={cardSize} value={1} type='house' />
+              </div>
+            </div>
+            <div id='player-area' className='player-area'>
+              <div className='deal-grid'>
+                <Card size={cardSize} value={9} type='house' />
+                <Card size={cardSize} value={5} type='house' />
+                <Card size={cardSize} value={2} type='house' />
+                <Card size={cardSize} value={8} type='house' />
+                <Card size={cardSize} value={6} type='house' />
+                <Card size={cardSize} value={1} type='house' />
+                <Card size={cardSize} value={10} type='house' />
+                <Card size={cardSize} value={2} type='house' />
+              </div>
+            </div>
           </div>
+
+          <div id='player-hand' className='hand'>
+            <Card size={cardSize} value={2} type='plus' />
+            <Card size={cardSize} value={4} type='plus' />
+            <Card size={cardSize} value={5} type='plus' />
+            <Card size={cardSize} value={3} type='minus' />
+          </div>
+
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
   }
