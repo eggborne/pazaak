31be28d164c9a5e3b082f77d06062b3fc7633af4
Michael Donovan <mike@eggborne.com>
2018-12-04 15:22:56 -0800

    Refine handleClickCard and hand/deck pregame actions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,11 +13,18 @@ window.innerWidth < window.innerHeight
   : shorterDimension = window.innerHeight;
 
 let cardSize = {};
+let mediumCardSize = {};
+let miniCardSize = {};
 
-let cardHeight = (window.innerHeight / 6) * 0.75;
+let cardHeight = (window.innerHeight / 6) * 0.8;
 
 cardSize.width = (cardHeight / 1.66);
 cardSize.height = cardHeight;
+mediumCardSize.width = cardSize.width * 0.85;
+mediumCardSize.height = cardSize.height * 0.85;
+miniCardSize.width = cardSize.width * 0.75;
+miniCardSize.height = cardSize.height * 0.75;
+
 
 class App extends React.Component {
   constructor(props) {
@@ -25,30 +32,45 @@ class App extends React.Component {
     this.state = {
       phase: 'splashScreen',
       deck: [],
+      cardSelection: [
+        { id: 11111, value: 1, type: 'plus' },
+        { id: 22222, value: 2, type: 'plus' },
+        { id: 33333, value: 3, type: 'plus' },
+        { id: 44444, value: 4, type: 'plus' },
+        { id: 55555, value: 5, type: 'plus' },
+        { id: 66666, value: 6, type: 'plus' },
+        { id: 77777, value: -1, type: 'minus' },
+        { id: 88888, value: -2, type: 'minus' },
+        { id: 99999, value: -3, type: 'minus' },
+        { id: 101010, value: -4, type: 'minus' },
+        { id: 111111, value: -5, type: 'minus' },
+        { id: 121212, value: -6, type: 'minus' },
+      ],
       userDeck: [
-        { id: 1, value: 1, type: 'plus' },
-        { id: 2, value: 2, type: 'plus' },
-        { id: 3, value: 3, type: 'plus' },
-        { id: 4, value: 3, type: 'plus' },
-        { id: 5, value: 4, type: 'plus' },
-        { id: 6, value: 5, type: 'plus' },
-        { id: 7, value: 6, type: 'plus' },
-        { id: 8, value: 2, type: 'minus' },
-        { id: 9, value: 3, type: 'minus' },
-        { id: 10, value: 5, type: 'minus' },
+        // { id: 1, value: 1, type: 'plus' },
+        // { id: 2, value: 2, type: 'plus' },
+        // { id: 3, value: 3, type: 'plus' },
+        // { id: 4, value: 3, type: 'plus' },
+        // { id: 5, value: 4, type: 'plus' },
+        // { id: 6, value: 5, type: 'plus' },
+        // { id: 7, value: 6, type: 'plus' },
+        // { id: 8, value: -2, type: 'minus' },
+        // { id: 9, value: -3, type: 'minus' },
+        // { id: 10, value: -5, type: 'minus' },
       ],
       opponentDeck: [
-        { id: 11, value: 1, type: 'plus' },
-        { id: 12, value: 3, type: 'plus' },
-        { id: 13, value: 3, type: 'plus' },
-        { id: 14, value: 4, type: 'plus' },
-        { id: 15, value: 5, type: 'plus' },
-        { id: 16, value: 1, type: 'minus' },
-        { id: 17, value: 2, type: 'minus' },
-        { id: 18, value: 4, type: 'minus' },
-        { id: 19, value: 4, type: 'minus' },
-        { id: 20, value: 6, type: 'minus' },
+        { id: 1, value: 1, type: 'plus' },
+        { id: 2, value: 3, type: 'plus' },
+        { id: 3, value: 3, type: 'plus' },
+        { id: 4, value: 4, type: 'plus' },
+        { id: 5, value: 5, type: 'plus' },
+        { id: 6, value: -1, type: 'minus' },
+        { id: 7, value: -2, type: 'minus' },
+        { id: 8, value: -4, type: 'minus' },
+        { id: 9, value: -4, type: 'minus' },
+        { id: 10, value: -6, type: 'minus' },
       ],
+      idCount: 11,
       userHand: [],
       opponentHand: [],
       userGrid: [],
@@ -73,28 +95,22 @@ class App extends React.Component {
     }
 
     this.state.deck = this.shuffleDeck(deck);
-    let hands = this.getPlayerHands();
-    this.state.userHand = hands[0];
-    this.state.opponentHand = hands[1];
 
     this.shuffleDeck = this.shuffleDeck.bind(this);
     this.getPlayerHands = this.getPlayerHands.bind(this);
-    this.dealToMainDeck = this.dealToMainDeck.bind(this);
+    this.dealToPlayerGrid = this.dealToPlayerGrid.bind(this);
     this.handleClickStart = this.handleClickStart.bind(this);
     this.handleClickHow = this.handleClickHow.bind(this);
     this.handleClickOptions = this.handleClickOptions.bind(this);
     this.handleClickPlay = this.handleClickPlay.bind(this);
-    this.dealToMainDeck = this.dealToMainDeck.bind(this);
     this.handleClickCard = this.handleClickCard.bind(this);
     this.handleClickEndTurn = this.handleClickEndTurn.bind(this);
     this.handleClickStand = this.handleClickStand.bind(this);
+    this.getCardIndexById = this.getCardIndexById.bind(this);
   }
 
   componentDidMount() {
     document.getElementById('container').style.height = window.innerHeight + 'px';
-    document.getElementById('deck-selection-grid').style.minWidth = `${cardSize.width*6}px`;
-    document.getElementById('deck-selection-grid').style.maxWidth = `${cardSize.width*6}px`;
-    // document.getElementById('deck-selection-grid').style.maxWidth = '300px';
     Array.from(document.getElementsByClassName('deal-grid')).map((el) => {
       el.style.width = `${(cardSize.width * 4) + (cardSize.height * 0.42)}px`;
       el.style.height = `${cardSize.height * 2.2}px`;
@@ -138,20 +154,23 @@ class App extends React.Component {
         opponentMinus.push(card);
       }
     });
-    // sort those arrays by value
+    // sort those arrays by value (negatives in reverse order)
     userPlus.sort((a, b) => a.value - b.value);
-    userMinus.sort(function (a, b) { return a.value - b.value; });
+    userMinus.sort(function (a, b) { return b.value - a.value; });
     opponentPlus.sort((a, b) => a.value - b.value);
-    opponentMinus.sort(function (a, b) { return a.value - b.value; });
+    opponentMinus.sort(function (a, b) { return b.value - a.value; });
     // put them back in the array, pluses first
     userPlus.map(card => newUserHand.push(card));
     userMinus.map(card => newUserHand.push(card));
     opponentPlus.map(card => newOpponentHand.push(card));
     opponentMinus.map(card => newOpponentHand.push(card));
-    return [newUserHand, newOpponentHand];
+    this.setState({
+      userHand: newUserHand,
+      opponentHand: newOpponentHand
+    });
   }
 
-  dealToMainDeck(player) {
+  dealToPlayerGrid(player) {
     if (player === 'user') {
       let newGridCards = this.state.userGrid.slice();
       let deckCopy = this.state.deck.slice();
@@ -159,7 +178,11 @@ class App extends React.Component {
       newGridCards.push(newCard);
       let newTotal = 0;
       newTotal += this.state.userTotal + newCard.value;
-      console.log(`new ${player} total ${newTotal}`);
+      if (newTotal === 20) {
+        document.getElementById('user-total').style.color = 'green';
+      } else if (newTotal > 20) {
+        document.getElementById('user-total').style.color = 'red';
+      }
       this.setState({
         userGrid: newGridCards,
         userTotal: newTotal
@@ -171,7 +194,11 @@ class App extends React.Component {
       newGridCards.push(newCard);
       let newTotal = 0;
       newTotal += this.state.opponentTotal + newCard.value;
-      console.log(`new ${player} total ${newTotal}`);
+      if (newTotal === 20) {
+        document.getElementById('opponent-total').style.color = 'green';
+      } else if (newTotal > 20) {
+        document.getElementById('opponent-total').style.color = 'red';
+      }
       this.setState({
         opponentGrid: newGridCards,
         opponentTotal: newTotal
@@ -184,6 +211,9 @@ class App extends React.Component {
     let clicked = event.target.id;
     Util.flash(clicked, 'color', '#5CB3FF', '#cc0');
     Util.flash(clicked, 'background-color', 'black', '#111');
+
+    document.getElementById('deck-select-footer').style.transform = 'translateY(0%)';
+
     setTimeout(() => {
       this.setState({
         phase: 'selectingDeck'
@@ -195,11 +225,18 @@ class App extends React.Component {
     let clicked = event.target.id;
     Util.flash(clicked, 'color', '#5CB3FF', '#cc0');
     Util.flash(clicked, 'background-color', 'black', '#111');
+
+    let hands = this.getPlayerHands();
+    // this.state.userHand = hands[0];
+    // this.state.opponentHand = hands[1];
+
+    document.getElementById('deck-select-footer').style.transform = 'transform: translateY(100%)';
+
     setTimeout(() => {
       this.setState({
         phase: 'gameStarted'
       });
-    }, 100);
+    }, 150);
   }
   handleClickHow(event) {
     event.preventDefault();
@@ -224,18 +261,13 @@ class App extends React.Component {
       });
     }, 150);
   }
-
-  handleClickCard(event) {
-    event.preventDefault();
-
-  }
   handleClickEndTurn(event) {
     event.preventDefault();
     let clicked = event.target.id;
     Util.flash(clicked, 'color', '#5CB3FF', '#cc0');
     Util.flash(clicked, 'background-color', 'black', '#111');
     if (this.state.userTotal < 20 && this.state.userGrid.length < 9) {
-      this.dealToMainDeck('user');
+      this.dealToPlayerGrid('user');
     }
   }
   handleClickStand(event) {
@@ -243,11 +275,51 @@ class App extends React.Component {
     let clicked = event.target.id;
     Util.flash(clicked, 'color', '#5CB3FF', '#cc0');
     Util.flash(clicked, 'background-color', 'black', '#111');
-
-
     if (this.state.opponentTotal < 20 && this.state.opponentGrid.length < 9) {
-      this.dealToMainDeck('opponent');
+      this.dealToPlayerGrid('opponent');
     }
+  }
+  handleClickCard(value, type) {
+    event.preventDefault();
+    if (this.state.phase === 'selectingDeck') {
+      if (event.target.id.toString().length > 9) {
+        // it's a selection card, so put it in player deck
+        if (this.state.userDeck.length < 10) {
+          let deckCopy = this.state.userDeck.slice();
+          let newCard = { id: this.state.idCount, value: value, type: type };
+          deckCopy.push(newCard);
+          let newCount = this.state.idCount;
+          if (deckCopy.length === 10) {
+            document.getElementById('play-button').classList.remove('disabled-button');
+          }
+          this.setState({
+            userDeck: deckCopy,
+            idCount: (this.state.idCount + 1)
+          });
+        }
+      } else {
+        // it's already in the player deck, so take it out
+        let clickedCardId = event.target.id;
+        let deckCopy = this.state.userDeck.slice();
+        let indextoRemove = this.getCardIndexById(deckCopy, clickedCardId);
+        deckCopy.splice(indextoRemove, 1);
+        if (deckCopy.length === 9) {
+          document.getElementById('play-button').classList.add('disabled-button');
+        }
+        this.setState({
+          userDeck: deckCopy
+        });
+      }
+    }
+  }
+  getCardIndexById(arr, id) {
+    let match = undefined;
+    arr.forEach((card, i) => {
+      if (`card-${card.id}` === id) {
+        match = i;
+      }
+    });
+    return match;
   }
 
   render() {
@@ -257,6 +329,10 @@ class App extends React.Component {
     let instructionsScreen = { display: 'none' };
     let optionsScreen = { display: 'none' };
     let deckSelectScreen = { display: 'none' };
+    let cardsLeft = 10 - this.state.userDeck.length;
+    let chooseStyle = {
+      transition: 'all 300ms ease'
+    };
     if (this.state.phase === 'gameStarted') {
       gameStarted = { display: 'flex' };
       footerOn = { position: 'relative', bottom: '0' };
@@ -269,44 +345,67 @@ class App extends React.Component {
     } else if (this.state.phase === 'splashScreen') {
       introScreen = { display: 'flex' };
     }
+    let cardPlural = 's';
+    if (cardsLeft === 1) {
+      cardPlural = '';
+    }
+    if (cardsLeft === 0) {
+      cardsLeft = 1;
+      cardPlural = '';
+      chooseStyle.opacity = 0;
+    }
     return (
       <div id='container'>
         <Header />
+
+        {/* INTRO */}
         <div style={introScreen} id='intro-screen'>
           <button onClick={this.handleClickStart} className='intro-button' id='start-button'>Start Game</button>
           <button onClick={this.handleClickHow} className='intro-button' id='how-button'>How to Play</button>
           <button onClick={this.handleClickOptions} className='intro-button' id='options-button'>Options</button>
         </div>
+
+        {/* INSTRUCTIONS */}
         <div style={instructionsScreen} id='instructions-screen'>
           How to play
         </div>
+
+        {/* OPTIONS */}
         <div style={optionsScreen} id='options-screen'>
           Options
         </div>
+
+        {/* DECK SELECT */}
         <div style={deckSelectScreen} id='deck-select-screen'>
           <div id='deck-select-title'>
-            Create deck
-            <div className='smaller'>choose 4 cards</div>
-            </div>
-          
+            <div className='shadowed-text'>Create deck</div>
+            <div style={chooseStyle} className='smaller shadowed-text'>choose {cardsLeft} card{cardPlural}</div>
+          </div>
           <div id='deck-selection-area'>
             <div id='deck-selection-grid'>
-              {this.state.userDeck.map((card, i) => 
-                <Card key={card.id} id={card.id} size={cardSize} value={card.value} type={card.type} />
+              {this.state.cardSelection.map((card, i) =>
+                <Card key={card.id} id={card.id} onClickCard={this.handleClickCard} size={mediumCardSize} value={card.value} type={card.type} />
               )}
             </div>
           </div>
-          <div id='preview-deck'>
-            HAND
+          <div className='smaller shadowed-text'>YOUR DECK:</div>
+          <div id='preview-deck-area'>
+            <div id='preview-deck-grid'>
+              {this.state.userDeck.map((card, i) =>
+                <Card className='cock' key={card.id} id={card.id} onClickCard={this.handleClickCard} size={cardSize} value={card.value} type={card.type} />
+              )}
+            </div>
           </div>
           <div id='deck-select-footer'>
-            <button onClick={this.handleClickPlay} className='intro-button' id='options-button'>Play!</button>
+            <button className='disabled-button' onClick={this.handleClickPlay} id='play-button'>Ready!</button>
           </div>
         </div>
+
+        {/* GAME BOARD */}
         <div style={gameStarted} id='game-board'>
           <div id='opponent-hand' className='hand'>
-            {this.state.opponentHand.map((card, i) => 
-              <CardBack key={i} size={cardSize} />
+            {this.state.opponentHand.map((card, i) =>
+              <CardBack key={i} size={miniCardSize} />
             )}
           </div>
           <div id='grids'>
@@ -342,6 +441,7 @@ class App extends React.Component {
             })}
           </div>
         </div>
+
         <Footer display={footerOn} onClickEndTurn={this.handleClickEndTurn} onClickStand={this.handleClickStand} />
       </div>
     );
