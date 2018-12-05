67103a33b26c3bf673e68515c1d985b2222f0076
Michael Donovan <mike@eggborne.com>
2018-12-05 15:31:17 -0800

    Make title text link to splash page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 function Header(props) {
   return (
     <div className='shadowed-text' id='header'>
-      Pazaak
+      <div id='header-link' onClick={props.onClickHeader}>Pazaak.online</div>
     </div>
   );
 }
 
 Header.propTypes = {
-
+  onClickHeader: PropTypes.func
 };
 
 
