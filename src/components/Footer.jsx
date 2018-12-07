999b85449060736db7214f4803feee16836a78f1
Michael Donovan <mike@eggborne.com>
2018-12-06 16:55:45 -0800

    Create hamburger element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,25 @@ import PropTypes from 'prop-types';
 
 function Footer(props) {
   return (
-    <div style={props.display} id='footer'>
-      <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
-      <button className='move-button' onClick={props.onClickStand} id='stand-button'>Stand</button>
+    <div style={props.style} id='footer'>
+      <div id='footer-button-area'>
+        <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
+        <button className='move-button' onClick={props.onClickStand} id='stand-button'>Stand</button>
+      </div>
+      <div onClick={props.onClickHamburger} id='hamburger'>
+        <div className='hamburger-bar' id='top-hamburger-bar'></div>
+        <div className='hamburger-bar'></div>
+        <div className='hamburger-bar' id='bottom-hamburger-bar'></div>
+      </div>
     </div>
   );
 }
 
 Footer.propTypes = {
+  style: PropTypes.object,
   onClickEndTurn: PropTypes.func,
   onClickStand: PropTypes.func,
-  display: PropTypes.object
+  onClickHamburger: PropTypes.func
 };
 
 
