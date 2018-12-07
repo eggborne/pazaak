db01b9ec9da28e816b7ec9a6e08ad5a7f900b05d
Michael Donovan <mike@eggborne.com>
2018-12-06 16:56:44 -0800

    Adjust styles

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,16 +1,19 @@
-import style from '../css/styles.css';
+// import style from '../css/styles.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function ResultModal(props) {
-
+  
   return (
     <div id='result-modal'>
       <div className='shadowed-text' id='result-title'>
         {props.titleText}
       </div>
       <div className='shadowed-text' id='result-body'>
-        {props.winner}
+        <div id='result-winner'>{props.winner}</div>
+        <div id='result-scores'>
+          {props.finalScores.user} - {props.finalScores.opponent}
+        </div>
       </div>
       <button onClick={(event) => props.onClickOKButton(event)} id='ok-button'>{props.buttonText}</button>
     </div>
@@ -20,8 +23,9 @@ function ResultModal(props) {
 ResultModal.propTypes = {
   titleText: PropTypes.string,
   winner: PropTypes.string,
+  finalScores: PropTypes.object,
   buttonText: PropTypes.string,
-  onClickOKButton: PropTypes.string
+  onClickOKButton: PropTypes.func
 };
 
 
