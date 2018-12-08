319b880c4d7fd4d2368bcb77be7485e50d6fe94f
Michael Donovan <mike@eggborne.com>
2018-12-08 00:15:22 -0800

    Adjust style

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -3,16 +3,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ResultModal(props) {
-  
+  let userScoreStyle = { color: 'white' };
+  let opponentScoreStyle = { color: 'white' };
+  let winnerStyle = { display: 'none' };
+  let scoresStyle = { display: 'block' };
+  if (!props.roundOver) {
+    if (props.winner === 'user') {
+      userScoreStyle = { color: 'green' };
+      opponentScoreStyle = { color: 'red' };
+    } else if (props.winner === 'opponent') {
+      userScoreStyle = { color: 'red' };
+      opponentScoreStyle = { color: 'green' };
+    }
+  } else {
+    winnerStyle = { display: 'block' };
+    scoresStyle = { display: 'none' };
+  }
   return (
     <div id='result-modal'>
       <div className='shadowed-text' id='result-title'>
         {props.titleText}
       </div>
       <div className='shadowed-text' id='result-body'>
-        <div id='result-winner'>{props.winner}</div>
-        <div id='result-scores'>
-          {props.finalScores.user} - {props.finalScores.opponent}
+        <div style={winnerStyle} id='result-winner'>{props.winner}</div>
+        <div style={scoresStyle} id='result-scores'>
+          {props.playerNames.user}: <span style={userScoreStyle}>{props.finalScores.user}</span> - {props.playerNames.opponent}: <span style={opponentScoreStyle}>{props.finalScores.opponent}</span>
         </div>
       </div>
       <button onClick={(event) => props.onClickOKButton(event)} id='ok-button'>{props.buttonText}</button>
@@ -22,7 +37,9 @@ function ResultModal(props) {
 
 ResultModal.propTypes = {
   titleText: PropTypes.string,
+  playerNames: PropTypes.object,
   winner: PropTypes.string,
+  roundOver: PropTypes.bool,
   finalScores: PropTypes.object,
   buttonText: PropTypes.string,
   onClickOKButton: PropTypes.func
