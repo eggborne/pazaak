e4c040fbfe22f2194dcc966ff414bf1adf90a279
Michael Donovan <mike@eggborne.com>
2018-12-08 00:14:30 -0800

    Add Hall of Fame button

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -5,9 +5,12 @@ import PropTypes from 'prop-types';
 function IntroScreen(props) {
   return (
     <div style={props.style} id='intro-screen'>
+      <div id='name-input-label' className='shadowed-text' >Player name</div>
+      <input name='player-name' id='player-name-input' placeholder='Player'></input>
       <button onClick={props.onClickStart} className='intro-button' id='start-button'>Start Game</button>
-      <button onClick={props.onClickHow} className='intro-button' id='how-button'>How to Play</button>
       <button onClick={props.onClickOptions} className='intro-button' id='options-button'>Options</button>
+      <button onClick={props.onClickHow} className='intro-button' id='how-button'>How to Play</button>
+      <button onClick={props.onClickHallOfFame} className='intro-button' id='hall-of-fame-button'>Hall of Fame</button>
     </div>
   );
 }
@@ -15,7 +18,8 @@ IntroScreen.propTypes = {
   style: PropTypes.object,
   onClickStart: PropTypes.func,
   onClickHow: PropTypes.func,
-  onClickOptions: PropTypes.func
+  onClickOptions: PropTypes.func,
+  onClickHallOfFame: PropTypes.func
 };
 
 export default IntroScreen;
