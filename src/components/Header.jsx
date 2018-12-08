8342730919a283fcd0c2fb7e9a7f2b59c12488c1
Michael Donovan <mike@eggborne.com>
2018-12-08 00:13:56 -0800

    Remove link to home screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Header(props) {
+function Header() {
   return (
     <div className='shadowed-text' id='header'>
-      <div id='header-link' onClick={props.onClickHeader}>Pazaak.online</div>
+      <div id='header-link'>Pazaak.online</div>
     </div>
   );
 }
-
-Header.propTypes = {
-  onClickHeader: PropTypes.func
-};
-
 
 export default Header;
