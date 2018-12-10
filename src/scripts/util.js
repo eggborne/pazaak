c86cbf512e77ad0652b4fbe2861a17ba20fb7eb0
Michael Donovan <mike@eggborne.com>
2018-12-10 12:29:38 -0800

    Add cookie functions

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -19,3 +19,32 @@ export function shuffle(arr) {
   arr.sort(() => Math.random() - 0.5);
   return arr;
 }
+
+export const setCookie = (cname, cvalue, exdays) => {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+};
+const getCookie = (cname) => {
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+export const checkCookie = () => {
+  var playerName = getCookie('username');
+  if (playerName != '') {
+    document.getElementById('player-name-input').value = playerName;
+    console.warn(`Recognized user as ${playerName}`);
+  }
+};
