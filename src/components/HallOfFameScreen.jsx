a08e95e779146412f9becc7e5c2d1e9b7ef2399b
Michael Donovan <mike@eggborne.com>
2018-12-14 19:56:57 -0800

    Add header and footer, adjust style

--- src/components/HallOfFameScreen.jsx
+++ src/components/HallOfFameScreen.jsx
@@ -5,24 +5,33 @@ import PropTypes from 'prop-types';
 function HallOfFameScreen(props) {
   return (
     <div style={props.style} id='hall-of-fame-screen'>
+      
       <div id='high-score-title' className='options-instructions-title shadowed-text'>Hall of Fame</div>
-      <div id='high-score-heading' className='shadowed-text'>
-        <div className='heading-label'>Name</div>
-        <div className='heading-label'>Sets<br />won</div>
-        <div className='heading-label'>Rounds<br />won</div>
-      </div>
-      <div className='shadowed-text' id='high-scores-list'>
-        {props.highScores.map((entry, i) => {
-          return (
-            <div key={i} id={entry.id} className='high-score-entry'>
-              <div>{entry.playerName}</div>
-              <div>{entry.setWins} / {entry.totalSets}</div>
-              <div>{entry.roundWins} / {entry.totalRounds}</div>
-            </div>);
-        })}
-      </div>
-      <button onClick={props.onClickBack} className='back-button' id='hall-of-fame-back-button'>Back</button>
 
+      <div id='high-score-area'>
+        
+        <div id='high-score-heading' className='shadowed-text'>
+          <div className='heading-label'>Name</div>
+          <div className='heading-label'>Sets<br />won</div>
+          <div className='heading-label'>Rounds<br />won</div>
+        </div>
+
+        <div className='shadowed-text' id='high-scores-list'>
+          {props.highScores.map((entry, i) => {
+            return (
+              <div key={i} id={entry.id} className='high-score-entry'>
+                <div>{entry.playerName}</div>
+                <div>{entry.setWins} / {entry.totalSets}</div>
+                <div>{entry.roundWins} / {entry.totalRounds}</div>
+              </div>);
+          })}
+        </div>
+        
+      </div>
+
+      <div className='option-footer'>
+        <button onClick={(event) => props.onClickBack(event, 'splashScreen')} className='back-button' id='hall-of-fame-back-button'>Back</button>
+      </div>
     </div>
   );
 }
