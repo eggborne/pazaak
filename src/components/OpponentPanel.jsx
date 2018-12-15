a785ea70d67e56e0312b5addd1179a8fbfd1297a
Michael Donovan <mike@eggborne.com>
2018-12-15 01:30:15 -0800

    Refine layout

--- src/components/OpponentPanel.jsx
+++ src/components/OpponentPanel.jsx
@@ -1,8 +1,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from './Card';
-import CardBack from './CardBack';
 
 function OpponentPanel(props) {
   let skillArray = Array.apply(null, Array(10)).map(function () { });
@@ -17,23 +15,27 @@ function OpponentPanel(props) {
       </div>
       <div className='opponent-description'>
         <div className='opponent-stats-grid'>
-          <div className='skill-label'>Skill Level</div>
-          <div className='difficulty-bar'>
-            {skillArray.map((entry, i) => {
-              let barClass = 'difficulty-bar-segment segment-filled';
-              if (i >= props.character.skillLevel) {
-                barClass = 'difficulty-bar-segment';
-              }
-              return <div key={i * (props.index + 1)} className={barClass}></div>;
-            })}
-          </div>
-          <div className='opponent-prize-label'>
-            Prize
+          <div className='difficulty-area'>
+            <div className='skill-label'>Skill Level</div>
+            <div className='difficulty-bar-body'>
+              {skillArray.map((entry, i) => {
+                let barClass = 'difficulty-bar-segment segment-filled';
+                if (i >= props.character.skillLevel) {
+                  barClass = 'difficulty-bar-segment';
+                }
+                return <div key={i * (props.index + 1)} className={barClass}></div>;
+              })}
+            </div>
           </div>
           <div className='opponent-prize-area'>
-            <div className='opponent-prize-credits'>{props.character.prize.credits}</div>&nbsp;<div>credits</div>
+            <div className='opponent-prize-label'>
+              Prize
+            </div>
+            <div className='opponent-prize-body'>
+              <div className='opponent-prize-credits'>{props.character.prize.credits}</div>&nbsp;<div>credits</div>
+            </div>
           </div>
-          <div className='opponent-prize-cards'>
+          {/* <div className='opponent-prize-cards'>
             {props.character.prize.cards.map((card, i) => {
               if (props.character.name !== 'theemperor') {
                 return <Card key={i * (props.index + 1)} id={i} size={props.cardSize} value={card.value} type={card.type} onClickCard={null} />;
@@ -41,16 +43,17 @@ function OpponentPanel(props) {
                 return <CardBack key={i * (props.index + 1)} id={i} size={props.cardSize} />;
               }
             })}
+          </div> */}
+          <div className='opponent-strategy-area'>
+            <div className='opponent-strategy-label'>Strategy</div>
+            <div className='opponent-strategy-list'>
+              <div key={0} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.stand.description}</div>
+              <div key={1} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.handCards.description}</div>
+              <div key={2} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.tie.description}</div>
+            </div>
           </div>
         </div>
-        <div className='opponent-strategy-area'>
-          <div className='opponent-strategy-label'>Strategy</div>
-          <div className='opponent-strategy-list'>
-            <div key={0} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.stand.description}</div>
-            <div key={1} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.handCards.description}</div>
-            <div key={2} className='opponent-strategy-item'><div className='bullet'>•</div>{props.character.strategy.tie.description}</div>
-          </div>
-        </div>
+
         {/* <div className='opponent-deck-preview'>
           <div className='opponent-deck-label'>Deck</div>
           <div className='opponent-deck-cards'>
