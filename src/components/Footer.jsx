87491948930bb82a9e1f33e2ca7805306d3c5d48
Michael Donovan <mike@eggborne.com>
2018-12-14 19:54:13 -0800

    Change to general page footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Hamburger from './Hamburger';
 
-function Footer(props) {
+
+function Footer() {
   return (
-    <div style={props.style} id='footer'>
-      <div id='footer-button-area'>
-        <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
-        <button className='move-button disabled-button hidden-button' onClick={props.onClickSwitchSign} id='switch-sign-button'>+/-</button>
-        <button className='move-button' onClick={props.onClickStand} id='stand-button'>Stand</button>
+    <div id='footer'>
+      <style jsx>{`
+        #footer {
+          position: absolute;
+          position: absolute;
+          box-sizing: border-box;
+          width: 100vw;
+          bottom: 0;
+          font-family: sans-serif;
+          /* height: 3rem; */
+          min-height: 8vmax;
+          max-height: 8vmax;
+          min-width: 320px;
+          background-color: var(--red-bg-color);
+          display: inline-flex;
+          align-items: center;
+          font-size: 1.5vmax;
+          transform: translateY(100%);
+          transition: all 300ms;
+        }
+        #footer-contents {
+          display: inline-flex;
+          align-items: center;
+          justify-content: space-around;
+          width: 100%;
+          transition: all 300ms;
+        }
+        #footer-contents > div {
+          text-align: center;
+          width: calc(100vw / 3);
+        }`}
+      </style>
+      <div id='footer-contents'>
+        <div><i className="fab fa-github footer-icon"></i><br /><a href='https://github.com/eggborne'>GitHub</a></div>
+        <div><i className="far fa-envelope footer-icon"></i><br /><a href='mailto:mike@eggborne.com'>Contact</a></div>
+        <div>
+          <form id='donate-button' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type='hidden' name='cmd' value='_s-xclick' />
+            <input type='hidden' name='hosted_button_id' value='GHAXRA5JU4Y2J' />
+            <input id='pp-button' type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif' border='0' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button' />
+          </form>
+        </div>
       </div>
-      <Hamburger onClickHamburger={props.onClickHamburger} />
     </div>
   );
 }
-
-Footer.propTypes = {
-  style: PropTypes.object,
-  onClickEndTurn: PropTypes.func,
-  onClickSwitchSign: PropTypes.func,
-  onClickStand: PropTypes.func,
-  onClickHamburger: PropTypes.func,
-};
-
 
 export default Footer;
