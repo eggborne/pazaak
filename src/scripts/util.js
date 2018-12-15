2dfc578f1af4c6867f4e0a881f282b87578f2cfa
Michael Donovan <mike@eggborne.com>
2018-12-14 20:04:52 -0800

    Add randomInt(), getCardSizes(), and full screen toggle functions

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -5,21 +5,23 @@ export function flash(elementId, property, origColor, flashColor, interval) {
   el.style[property] = flashColor;
   setTimeout(() => {
     el.style[property] = origColor;
-    setTimeout(() => {
-      el.style[property] = flashColor;
-      setTimeout(() => {
-        el.style[property] = origColor;
-        el.style.pointerEvents = 'all';
-      }, interval);
-    }, interval);
+    el.style.pointerEvents = 'all';
+    // setTimeout(() => {
+    //   el.style[property] = flashColor;
+    //   // setTimeout(() => {
+    //   //   el.style[property] = origColor;
+    //   //   el.style.pointerEvents = 'all';
+    //   // }, interval);
+    // }, interval);
   }, interval);
 }
-
 export function shuffle(arr) {
   arr.sort(() => Math.random() - 0.5);
   return arr;
 }
-
+export function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 export const setCookie = (cname, cvalue, exdays) => {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -48,3 +50,63 @@ export const checkCookie = () => {
     console.warn(`Recognized user as ${playerName}`);
   }
 };
+
+export function getCardSizes() {
+  let cardSize = {};
+  let mediumCardSize = {};
+  let miniCardSize = {};
+  let microCardSize = {};
+  // let cardHeight = Math.round((window.innerHeight / 6) * 0.82);
+  let cardHeight = Math.round((window.innerHeight / 6) * 0.85);
+  // cardSize.width = Math.round(cardHeight / 1.55);
+  cardSize.width = (cardHeight / 1.66);
+  if ((window.innerWidth / cardHeight) < 3.5) {
+    cardHeight = Math.round((window.innerHeight / 6) * 0.83);
+    cardSize.width = (cardHeight / 1.68);
+  }
+  cardSize.height = cardHeight;
+  mediumCardSize.width = Math.round(cardSize.width * 0.85);
+  mediumCardSize.height = Math.round(cardSize.height * 0.85);
+  miniCardSize.width = Math.round(cardSize.width * 0.75);
+  miniCardSize.height = Math.round(cardSize.height * 0.75);
+  microCardSize.width = Math.round(cardSize.width * 0.6);
+  microCardSize.height = Math.round(cardSize.height * 0.6);
+
+  let cardSizes = [cardSize, mediumCardSize, miniCardSize, microCardSize];
+  cardSizes.map((sizeObj) => {
+    let cardHeight = sizeObj.height;
+    sizeObj.borderSize = `${Math.round(cardHeight / 100)}px`;
+    sizeObj.arrowBorderSize = `${Math.round(cardHeight / 10)}px`;
+    sizeObj.borderRadius = `${Math.round(cardHeight / 18)}px`;
+    sizeObj.bandRadius = `${Math.round(cardHeight / 24)}px`;
+    sizeObj.badgeRadius = `${Math.round(cardHeight / 36)}px`;
+    sizeObj.margin = `${Math.round(cardHeight * 0.015)}px`;
+    sizeObj.bubbleSize = `${Math.round(cardHeight * 0.16)}px`;
+    sizeObj.backBubbleSize = `${Math.round(cardHeight * 0.18)}px`;
+    sizeObj.fontSize = `${(cardHeight * 0.17)}px`;
+  });
+  let cardsObj = {};
+  cardsObj.cardSize = cardSizes[0];
+  cardsObj.mediumCardSize = cardSizes[1];
+  cardsObj.miniCardSize = cardSizes[2];
+  cardsObj.microCardSize = cardSizes[3];
+  return cardsObj;
+}
+function fullScreenCall() {
+  var root = document.body;
+  return root.requestFullscreen || root.webkitRequestFullscreen || root.mozRequestFullScreen || root.msRequestFullscreen;
+}
+function exitFullScreenCall() {
+  return document.exitFullscreen || document.webkitExitFullscreen || document.mozCancelFullScreen || document.msExitFullscreen;
+}
+function isFullScreen() {
+  return document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+}
+export function toggleFullScreen() {
+  if (!isFullScreen()) {
+    fullScreenCall().call(document.getElementById('container'));
+  } else {
+    exitFullScreenCall().call(document);
+  }
+}
+
