fe6e1031a729d9e4c52fb86de2e1ea2c79d3075c
Michael Donovan <mike@eggborne.com>
2018-12-15 01:28:08 -0800

    Convert card selection and deck preview areas to display: grid

--- src/components/DeckSelectScreen.jsx
+++ src/components/DeckSelectScreen.jsx
@@ -5,7 +5,7 @@ import Card from './Card';
 
 function DeckSelectionScreen(props) {
   let cardsLeft = 10 - props.userDeck.length;
-  let chooseStyle = { transition: 'all 300ms ease'};
+  let chooseStyle = { transition: 'all 300ms ease' };
   let chooseText = 'Ready to play!';
   let cardPlural = 's';
   if (cardsLeft === 1) {
@@ -15,14 +15,45 @@ function DeckSelectionScreen(props) {
     cardsLeft = 1;
     cardPlural = '';
     chooseStyle.color = 'green';
-    
+
     // document.getElementById('choose-text').innerHTML = ;
   } else {
     chooseText = `choose ${cardsLeft} card${cardPlural}`;
     // document.getElementById('choose-text').innerHTML = 
   }
+  let deckSelectGrid = [];
+  props.userDeck.map((card, i) => {
+    deckSelectGrid.push(<Card key={i} id={card.id} onClickCard={props.onClickCard} size={props.cardSize} value={card.value} type={card.type} />);
+  });
+  let gridWidth = props.mediumCardSize.width + (parseFloat(props.mediumCardSize.borderSize) * 2);
+  let gridHeight = props.mediumCardSize.height + (parseFloat(props.mediumCardSize.borderSize) * 2);
+  let previewGridWidth = props.cardSize.width + (parseFloat(props.cardSize.borderSize) * 2);
+  let previewGridHeight = props.cardSize.height + (parseFloat(props.cardSize.borderSize) * 2);
   return (
     <div style={props.style} id='deck-select-screen'>
+      <style jsx>{`
+        #deck-selection-grid {
+          /* box-sizing: border-box; */
+          display: grid;
+          grid-template-columns: ${gridWidth}px ${gridWidth}px ${gridWidth}px ${gridWidth}px ${gridWidth}px ${gridWidth}px;
+          grid-template-rows: ${gridHeight}px ${gridHeight}px;
+          grid-column-gap: 1vw;
+          grid-row-gap: 1vh;
+          margin-top: 0.5rem;
+        }
+        #preview-deck-grid {
+          display: grid;
+          grid-template-columns: ${previewGridWidth}px ${previewGridWidth}px ${previewGridWidth}px ${previewGridWidth}px ${previewGridWidth}px;
+          grid-template-rows: ${previewGridHeight}px ${previewGridHeight}px;
+          grid-row-gap: 0.25rem;
+          grid-column-gap: 0.75vh;
+        }
+        #preview-deck-grid  > div {
+          border-radius: ${props.cardSize.borderRadius};
+          background-color: rgb(105, 115, 128) !important;
+        }
+      `}
+      </style>
       <div id='deck-select-title'>
         <div className='shadowed-text'>Create deck</div>
         <div id='choose-text' style={chooseStyle} className='smaller shadowed-text'>{chooseText}</div>
@@ -37,9 +68,16 @@ function DeckSelectionScreen(props) {
       <div id='preview-deck-area'>
         <div id='preview-deck-title' className='smaller shadowed-text'>YOUR DECK:</div>
         <div id='preview-deck-grid'>
-          {props.userDeck.map((card) =>
-            <Card className='cock' key={card.id} id={card.id} onClickCard={props.onClickCard} size={props.cardSize} value={card.value} type={card.type} />
-          )}
+          <div>{deckSelectGrid[0]}</div>
+          <div>{deckSelectGrid[1]}</div>
+          <div>{deckSelectGrid[2]}</div>
+          <div>{deckSelectGrid[3]}</div>
+          <div>{deckSelectGrid[4]}</div>
+          <div>{deckSelectGrid[5]}</div>
+          <div>{deckSelectGrid[6]}</div>
+          <div>{deckSelectGrid[7]}</div>
+          <div>{deckSelectGrid[8]}</div>
+          <div>{deckSelectGrid[9]}</div>
         </div>
       </div>
       <div id='randomize-area'><button onClick={props.onClickRandomize} id='randomize-button'>Randomize</button></div>
