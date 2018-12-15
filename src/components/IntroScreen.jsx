9d386dfd74290dfbaf62be6e1f1d919089f81c7a
Michael Donovan <mike@eggborne.com>
2018-12-14 20:00:47 -0800

    Adjust style, add Footer component

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -1,17 +1,21 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import Footer from './Footer';
 
 function IntroScreen(props) {
   return (
     <div style={props.style} id='intro-screen'>
-      <div id='name-input-label' className='shadowed-text' >Player name</div>
-      <input name='player-name' id='player-name-input' placeholder='Player'></input>
-      <button onClick={props.onClickStart} className='intro-button' id='start-button'>Start Game</button>
+      <div id='name-input-area'>
+        <div id='name-input-label'>Player name</div>
+        <input name='player-name' id='player-name-input' placeholder='Enter name'></input>
+      </div>
+      <button onClick={props.onClickStart} className='intro-button' id='start-button'>Play!</button>
       <button onClick={props.onClickOptions} className='intro-button' id='options-button'>Options</button>
       <button onClick={props.onClickHow} className='intro-button' id='how-button'>How to Play</button>
       <button onClick={props.onClickHallOfFame} className='intro-button' id='hall-of-fame-button'>Hall of Fame</button>
-    </div>
+      <Footer />
+    </div >
   );
 }
 IntroScreen.propTypes = {
