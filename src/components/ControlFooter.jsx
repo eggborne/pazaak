b45005949834b643e964bcf60b5851c922070d72
Michael Donovan <mike@eggborne.com>
2018-12-21 23:09:12 -0800

    Add jsx styling, 'showing' prop

--- src/components/ControlFooter.jsx
+++ src/components/ControlFooter.jsx
@@ -3,8 +3,39 @@ import PropTypes from 'prop-types';
 import Hamburger from './Hamburger';
 
 function Footer(props) {
+  let extraClass;
+  if (props.showing) {
+    extraClass = 'control-footer-on';
+  }
   return (
-    <div style={props.style} id='control-footer'>
+    <div id='control-footer' className={extraClass}>
+      <style jsx>{`
+        #control-footer {
+          width: 100%;
+          box-sizing: border-box;
+          padding: 0.5vmax;
+          height: 10vmax;
+          min-height: 10vmax;
+          max-height: 10vmax;
+          background-color: var(--red-bg-color);
+          display: inline-flex;
+          justify-content: space-between;
+          align-items: stretch;
+          position: absolute;
+          bottom: -10vmax;
+          transition: all 300ms ease;
+        }
+        #control-footer-button-area {
+          display: inline-flex;
+          align-items: stretch;
+          justify-content: space-between;
+          flex-grow: 1; 
+        }
+        .control-footer-on {
+          position: relative !important;
+          bottom: 0 !important;
+        }
+      `}</style>
       <div id='control-footer-button-area'>
         <button className='move-button' onClick={props.onClickEndTurn} id='end-turn-button'>End Turn</button>
         <button className='move-button disabled-button hidden-button' onClick={props.onClickSwitchSign} id='switch-sign-button'>+/-</button>
@@ -16,6 +47,7 @@ function Footer(props) {
 }
 
 Footer.propTypes = {
+  showing: PropTypes.bool,
   style: PropTypes.object,
   onClickEndTurn: PropTypes.func,
   onClickSwitchSign: PropTypes.func,
