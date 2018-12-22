74f8148f81bbb8e1e6bb6a96531ca859f011e16c
Michael Donovan <mike@eggborne.com>
2018-12-21 23:12:06 -0800

    Add ppButtonSrc var, adjust style, remove icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 
 function Footer() {
+  let ppButtonSrc = 'https://pazaak.online/assets/images/ppbutton.gif';
   return (
     <div id='footer'>
       <style jsx>{`
         #footer {
-          position: absolute;
-          position: absolute;
+          position: fixed;
           box-sizing: border-box;
           width: 100vw;
           bottom: 0;
           font-family: sans-serif;
           /* height: 3rem; */
-          min-height: 6vmax;
-          max-height: 6vmax;
+          min-height: 6vh;
+          max-height: 6vh;
           min-width: 320px;
           background-color: var(--red-bg-color);
           display: inline-flex;
@@ -33,16 +33,17 @@ function Footer() {
         #footer-contents > div {
           text-align: center;
           width: calc(100vw / 3);
-        }`}
+        }
+      `}
       </style>
       <div id='footer-contents'>
-        <div><i className="fab fa-github footer-icon"></i><br /><a href='https://github.com/eggborne'>GitHub</a></div>
-        <div><i className="far fa-envelope footer-icon"></i><br /><a href='mailto:mike@eggborne.com'>Contact</a></div>
+        <div><a href='https://github.com/eggborne'>GitHub</a></div>
+        <div><a href='mailto:mike@eggborne.com'>Contact</a></div>
         <div>
           <form id='donate-button' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type='hidden' name='cmd' value='_s-xclick' />
             <input type='hidden' name='hosted_button_id' value='GHAXRA5JU4Y2J' />
-            <input id='pp-button' type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif' border='0' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button' />
+            <input id='pp-button' src={ppButtonSrc} type='image' border='0' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button' />
           </form>
         </div>
       </div>
