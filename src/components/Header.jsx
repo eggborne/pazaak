e3d5425c207c6dda226644af2945ad88f466c2b2
Michael Donovan <mike@eggborne.com>
2018-12-21 23:17:21 -0800

    Add jsx styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,21 @@ import React from 'react';
 function Header() {
   return (
     <div id='header'>
+      <style jsx>{`
+        #header {
+          font-family: 'Bungee';
+          min-height: var(--header-height);
+          max-height: var(--header-height);
+          background-color: var(--red-bg-color);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: var(--header-text-size);
+          transform: translateY(-100%);
+          color: white;
+          transition: all 300ms, letter-spacing 400ms, color 800ms, text-shadow 1000ms;
+        }
+      `}</style>
       <div id='header-link'>Pazaak.online</div>
     </div>
   );
