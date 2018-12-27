61b6c1ba807391ee363f6af268b5a1dc5a3659b9
Michael Donovan <mike@eggborne.com>
2018-12-26 17:15:25 -0800

    change checkCookie() to update App.state with cookieId if found, change cardSize

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,10 +1,10 @@
-export function shuffle(arr) {
+export const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
   return arr;
-}
-export function randomInt(min, max) {
+};
+export const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 export const setCookie = (cname, cvalue, exdays) => {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -26,11 +26,21 @@ const getCookie = (cname) => {
   }
   return '';
 };
-export const checkCookie = () => {
-  var playerName = getCookie('username');
-  if (playerName != '') {
+export const checkCookie = (app) => {
+  let response = getCookie('username');
+  if (response != '') {
+    let playerName = response.split('-')[0];
+    let uniqueId = response.split('-')[1];
+    let userStatusCopy = Object.assign({}, app.state.userStatus);
+    userStatusCopy.cookieId = parseInt(uniqueId);
+    app.setState({
+      userStatus: userStatusCopy
+    });
     document.getElementById('player-name-input').value = playerName;
-    console.warn(`Recognized user as ${playerName}`);
+    console.error(`Recognized user as ${response}. Filling name input and calling evaluatePlayerName()...`);
+    app.evaluatePlayerName(playerName, uniqueId);        
+  } else {
+    console.error('No cookie found.');
   }
 };
 
@@ -40,25 +50,25 @@ export function getCardSizes() {
   let mediumCardSize = {};
   let miniCardSize = {};
   let microCardSize = {};
-  let actualHeight;
-  actualHeight = window.innerHeight;
-  // let cardHeight = Math.round((window.innerHeight / 6) * 0.775);
-  let cardHeight = Math.round((actualHeight / 6) * 0.805);
+  let actualHeight = window.innerHeight;
+  let cardHeight = Math.round((window.innerHeight / 6) * 0.775);
+  // let cardHeight = Math.round((actualHeight / 6) * 0.805);
+  // let cardHeight = Math.round((actualHeight / 6) * 0.8);
   // cardSize.width = Math.round(cardHeight / 1.55);
   cardSize.width = Math.round(cardHeight / 1.55);
   let cardsPerWidth = window.innerWidth / cardSize.width;
   if (cardsPerWidth < 6) {
     console.warn('cardSize too wide at', cardSize.width);
-    let marg = window.innerWidth * 0.005;
+    let marg = window.innerWidth * 0.006;
     let maxWidth = (window.innerWidth / 5) - (marg * 5);
     cardSize.width = Math.round(maxWidth);
     console.warn('changed width to', cardSize.width);
-  } else {
-    console.warn('width okay with cardsPerWidth', cardSize.width);
   }
   cardSize.height = cardHeight;
-  mediumCardSize.width = Math.round(cardSize.width * 0.85);
-  mediumCardSize.height = Math.round(cardSize.height * 0.85);
+  // mediumCardSize.width = Math.round(cardSize.width * 0.85);
+  // mediumCardSize.height = Math.round(cardSize.height * 0.85);
+  mediumCardSize.width = Math.round(cardSize.width * 0.875);
+  mediumCardSize.height = Math.round(cardSize.height * 0.875);
   miniCardSize.width = Math.round(cardSize.width * 0.75);
   miniCardSize.height = Math.round(cardSize.height * 0.75);
   microCardSize.width = Math.round(cardSize.width * 0.6);
@@ -87,21 +97,21 @@ export function getCardSizes() {
   return cardsObj;
 }
 
-export function sizeElements(app, initialStart, cardSizes = app.state.cardSizes) {
-  console.error('SIZING ELEMENTS');
-  let startTime = window.performance.now();
+export function sizeElements(app, initialStart) {
   if (initialStart) {
-    // size button text
+  // size button text    
     Array.from(document.getElementsByClassName('intro-button')).map((button) => {
       let buttonTextLength = button.children[0].innerHTML.length;
+      if (button.children[0].innerHTML === 'How to Play') {
+        buttonTextLength = 7;
+      }
       let fontSize = (button.offsetWidth / (buttonTextLength)) + 'px';
       button.style.fontSize = fontSize;
       button.style.height = (window.innerHeight / 10) + 'px';
     });
   }
-  document.getElementById('container').style.height = `${window.innerHeight}px`;
-  let endTime = window.performance.now();
-  console.warn('sized elements in', (endTime - startTime));
+  // is this needed?
+  // document.getElementById('container').style.height = `${window.innerHeight}px`;
 }
 function fullScreenCall() {
   var root = document.body;
