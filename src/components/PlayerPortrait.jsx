ef83a1276008246c451dd63674fd38043e494aa0
Michael Donovan <mike@eggborne.com>
2019-01-11 16:55:44 -0800

    Adjust styling to accomodate new avatarsheet size

--- src/components/PlayerPortrait.jsx
+++ src/components/PlayerPortrait.jsx
@@ -2,24 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function PlayerPortrait(props) {
-  let sheetWidth = props.size * 6;
+  let sheetWidth = props.size * 8;
   let portraitSize = Math.floor(props.size);
   let extraClass = 'left-side';
   let displayLabel = '';
+  let labelOpacity = '0.5';
+  let labelAlign = 'center';
+  let labelFont = 'Nova Square';
+  let opacity = 1;
+  //let labelFontSize = props.size / 7;
+  let labelFontSize;
   if (props.type === 'mini') {
     extraClass = '';
+  } else if (props.type === 'micro') {
+    extraClass = '';
+    opacity = '0.5 !important';
   }
   if (!props.displayName) {
     displayLabel = 'display-none';
   }
-  let backgroundPositionX = -props.spriteIndex * (props.size);
-  let userPortrait = '';
-  if (props.spriteIndex === -1) { // custom avatar
-    backgroundPositionX = 0;
+  let spriteIndex = props.spriteIndex;
+  let backgroundPositionX = -spriteIndex * (props.size) + 'px';
+  let backgroundPositionY = 'top';
+  if (spriteIndex > 7) {
+    backgroundPositionX = -(spriteIndex % 8) * (props.size) + 'px';
+    backgroundPositionY = -props.size + 'px';
+    if (spriteIndex > 15) {
+      backgroundPositionY = (-props.size*2) + 'px';
+    }
   }
-  return (
-    <div className={`player-portrait ${extraClass} ${userPortrait}`}>
-      <style jsx>{`
+  let userPortrait = '';
+  
+  if (spriteIndex === -1) { // not logged in
+    backgroundPositionX = portraitSize;
+  }
+  let labelTextColor = 'white';
+  if (props.isSelf) {
+    labelTextColor = 'var(--option-on-color)';
+  }
+  if (props.source)
+    return (
+      <div className={`player-portrait ${extraClass} ${userPortrait}`}>
+        <style jsx>{`
         .player-portrait {
           position: relative;
           border-radius: ${props.size/10}px;
@@ -27,45 +51,35 @@ function PlayerPortrait(props) {
           display: flex;
           align-items: flex-end;
           background-image: url(${props.source});
-          background-size: ${sheetWidth}px ${portraitSize}px;
+          background-size: ${sheetWidth}px ${portraitSize*3}px;
           background-repeat: no-repeat;
           width: ${portraitSize}px;
           height: ${portraitSize}px;
-          background-position-x: ${backgroundPositionX}px;
-          background-position-y: center;
+          background-position-x: ${backgroundPositionX};
+          background-position-y: ${backgroundPositionY};
+          opacity: ${opacity};
         }
         .player-portrait-label {
-          font-family: 'Nova Square';
-          font-size: ${props.size/7}px;
+          font-family: ${labelFont};
+          font-size: ${labelFontSize}px;
           width: 100%;
-          background-color: rgba(0, 0, 0, 0.5);
+          background-color: rgba(0, 0, 0, ${labelOpacity});
           box-sizing: border-box;
-          border-radius: 0 0 0.5rem 0.5rem;
           display: flex;
           align-items: center;
           padding: 5%;
-          justify-content: center;
-        }
-        .mini-portrait-label {
-          box-sizing: border-box;
-          padding: 0.1rem 0.25rem;
-          font-size: 0.8rem;
-          font-family: 'Nova Square';
-          width: 100%;
-          background-color: rgba(0, 0, 0, 0.5);
-          box-sizing: border-box;
-          border-radius: 0 0 0.25rem 0.25rem;
-          display: flex;
-          align-items: center;
-          justify-content: center;
+          justify-content: ${labelAlign};
+          border-radius: 0 0 0.5rem 0.5rem;
+          color: ${labelTextColor} !important;
         }
       `}
-      </style>
-      <div className={`player-portrait-label ${displayLabel}`}>{props.displayName}</div>
-    </div>
-  );
+        </style>
+        <div className={`player-portrait-label ${displayLabel}`}>{props.displayName}</div>
+      </div>
+    );
 }
 PlayerPortrait.propTypes = {
+  isSelf: PropTypes.bool,
   size: PropTypes.number,
   type: PropTypes.string,
   source: PropTypes.string,
