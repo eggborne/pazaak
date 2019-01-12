cd88f6e66d2bdd88aa60742fa5112bc3a46673aa
Michael Donovan <mike@eggborne.com>
2019-01-11 16:53:41 -0800

    Add human-select-area and associated onClick props

--- src/components/OpponentSelectScreen.jsx
+++ src/components/OpponentSelectScreen.jsx
@@ -1,10 +1,32 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import OpponentPanel from './OpponentPanel';
+import CPUOpponentPanel from './CPUOpponentPanel';
+import HumanOpponentPanel from './HumanOpponentPanel';
 
+const statuses = {
+  splashScreen: 'at title screen',
+  selectingOpponent: 'selecting opponent',
+  showingOptions: 'at options screen',
+  selectingDeck: 'selecting deck',
+  showingHallOfFame: 'at Hall of Fame',
+  showingInstructions: 'at How to Play',
+  gameStarted: 'playing'
+};
 function OpponentSelectScreen(props) {
+  let userStatus = props.userStatus;
+  let usersHere = props.usersHere.slice();  
+  let highScores = props.highScores.slice();  
   let characterArray = [];
+  let loadingIconSource = 'https://pazaak.online/assets/images/loadingicon.png';
+  let scoreObj = {};
+  highScores.map((recordObj, i) => {
+    usersHere.map((userObj, p) => {
+      if (recordObj.id === parseInt(userObj.userId)) {
+        userObj.highScoresIndex = i;
+      }
+    });
+  });
   Object.entries(props.characters).map((entry, i) => {
     characterArray[i] = entry[0];
   });
@@ -31,6 +53,17 @@ function OpponentSelectScreen(props) {
           overflow-y: scroll;
           flex-grow: 1;
         }
+        #human-select-area {
+          padding: 2vw;
+          //display: none;
+        }
+        .opponent-type-label {
+          font-size: 1em;
+          line-height: 100%;
+          width: 100%;
+          height: 1rem;
+          text-align: center;
+        }
         .pre-footer {
           border-top: 1px solid black;
         }
@@ -46,14 +79,45 @@ function OpponentSelectScreen(props) {
             transform: scale(1.05);
           }
         }
+        #nobody-here-message {
+          width: 100%;
+          text-align: center;
+          margin-top: 1rem;
+          margin-bottom: 1rem;
+          color: #333;
+          text-shadow: none;
+        }
       `}</style>
       <div className='pre-header' id='opponent-select-title'>
         <div className='shadowed-text'>Choose Your Opponent</div>
       </div>
-
       <div id='opponent-select-area' className='shadowed-text'>
+        <div id='human-select-area'>
+          <h3 className='opponent-type-label'>Actual humans</h3>
+          {usersHere.length === 0 && <div id='human-opponents-loading-message' className='loading-message'>
+            <div id='human-opponents-loading-text' className='loading-text'>LOADING</div>
+            <img src={loadingIconSource} id='human-opponents-loading-icon' className='loading-icon' />
+          </div>}
+          {usersHere.length === 1 && <div id='nobody-here-message'>Nobody else is here :(</div>}
+          {usersHere.map((userObj, i) => 
+            (parseInt(userObj.userId) !== props.userId) &&
+            <HumanOpponentPanel key={i}
+              characters={props.characters}
+              isSelf={false}
+              cardSize={props.cardSize}
+              userObject={userObj}
+              scoreObject={highScores[userObj.highScoresIndex]}
+              opponentSelected={userObj.opponent}
+              statuses={statuses}
+              onClickMoreInfo={props.onClickMoreInfo}
+              onClickSendMessage={props.onClickSendMessage}
+              onClickRequestMatch={props.onClickRequestMatch}
+            />
+          )}
+        </div>
+        <h3 className='opponent-type-label'>CPU Players</h3>
         {characterArray.map((character, i) =>
-          <OpponentPanel key={i}
+          <CPUOpponentPanel key={i}
             selected={(props.opponentSelected === character)}
             cardSize={props.cardSize}
             index={i}
@@ -72,11 +136,17 @@ function OpponentSelectScreen(props) {
 OpponentSelectScreen.propTypes = {
   style: PropTypes.object,
   characters: PropTypes.object,
+  userStatus: PropTypes.object,
+  highScores: PropTypes.array,
   opponentSelected: PropTypes.string,
   cardSize: PropTypes.object,
   onClickOpponentReady: PropTypes.func,
   onClickPanel: PropTypes.func,
-  onClickBack: PropTypes.func
+  onClickBack: PropTypes.func,
+  usersHere: PropTypes.array,
+  userId: PropTypes.number,
+  onClickMoreInfo: PropTypes.func,
+  onClickRequestMatch: PropTypes.func
 };
 
 export default OpponentSelectScreen;
