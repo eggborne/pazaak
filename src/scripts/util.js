6a4d887eca2ead23865bd6ef03eeeceaa7862756
Michael Donovan <mike@eggborne.com>
2019-02-22 13:48:34 -0800

    Add getStarWarsName() and color-related functions

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,4 +1,18 @@
 
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const randomConsonant = () => consonants[randomInt(0, consonants.length - 1)];
+const randomVowel = () => vowels[randomInt(0, vowels.length - 1)];
+
+export const getStarWarsName = () => {
+  let firstName;
+  let lastName;
+  firstName = `${randomConsonant().toUpperCase()}${randomVowel()}${randomConsonant()}`;
+  lastName = `${randomConsonant().toUpperCase()}${randomVowel()}${randomConsonant()}${randomConsonant()}${randomVowel()}${randomConsonant()}`;
+  return `${firstName} ${lastName}`;
+}
+
 
 export const getPageLoadInfo = () => {
   let perfEntries = performance.getEntriesByType('navigation');
@@ -71,14 +85,17 @@ export const checkCookie = (app) => {
       app.evaluatePlayerName(playerName);
     });
   } else {
+    userStatusCopy.loggedInAs = 'Guest';
     app.setState({
-      checkedCookie: true
+      checkedCookie: true,
+      userStatus: userStatusCopy
     });
-    document.getElementById('avatar-row').style.opacity = 1;
+    //document.getElementById('avatar-row').style.opacity = 1;
     console.error('No cookie found. Leaving state.userStatus.cookieId undefined. Animating starfield.');
     // document.getElementById('container').style.backgroundImage = 'url(https://pazaak.online/assets/images/starfield.png)';
     document.getElementById('player-name-input').disabled = false;
     document.getElementById('starfield').play();
+
     // document.getElementById('initial-loading-message').innerHTML += `<br />User not recognized.`;
   }
 };
@@ -125,48 +142,48 @@ export const getTimeSinceFromSeconds = sessionLengthInSeconds => {
 };
 
 export function getColorValues(color) {
-  let values = { r:null, g:null, b:null, a:null };
-  if( typeof color == 'string' ){
+  let values = { r: null, g: null, b: null, a: null };
+  if (typeof color == 'string') {
     /* hex */
-    if( color.indexOf('#') === 0 ){
+    if (color.indexOf('#') === 0) {
       color = color.substr(1);
-      if( color.length == 3 )
+      if (color.length == 3)
         values = {
-          r:   parseInt( color[0]+color[0], 16),
-          g: parseInt( color[1]+color[1], 16),
-          b:  parseInt( color[2]+color[2], 16),
+          r: parseInt(color[0] + color[0], 16),
+          g: parseInt(color[1] + color[1], 16),
+          b: parseInt(color[2] + color[2], 16),
           a: 1
         };
       else
         values = {
-          r:   parseInt( color.substr(0,2), 16),
-          g: parseInt( color.substr(2,2), 16),
-          b:  parseInt( color.substr(4,2), 16),
+          r: parseInt(color.substr(0, 2), 16),
+          g: parseInt(color.substr(2, 2), 16),
+          b: parseInt(color.substr(4, 2), 16),
           a: 1
         };
       /* rgb */
-    }else if( color.indexOf('rgb(') === 0 ){
+    } else if (color.indexOf('rgb(') === 0) {
       let pars = color.indexOf(',');
       values = {
-        r:   parseInt(color.substr(4,pars)),
-        g: parseInt(color.substr(pars+1,color.indexOf(',',pars))),
-        b:  parseInt(color.substr(color.indexOf(',',pars+1)+1,color.indexOf(')'))),
+        r: parseInt(color.substr(4, pars)),
+        g: parseInt(color.substr(pars + 1, color.indexOf(',', pars))),
+        b: parseInt(color.substr(color.indexOf(',', pars + 1) + 1, color.indexOf(')'))),
         a: 1
       };
       /* rgba */
-    }else if( color.indexOf('rgba(') === 0 ){
+    } else if (color.indexOf('rgba(') === 0) {
       let pars = color.indexOf(','),
-        repars = color.indexOf(',',pars+1);
+        repars = color.indexOf(',', pars + 1);
       values = {
-        r:   parseInt(color.substr(5,pars)),
-        g: parseInt(color.substr(pars+1,repars)),
-        b:  parseInt(color.substr(color.indexOf(',',pars+1)+1,color.indexOf(',',repars))),
-        a: parseFloat(color.substr(color.indexOf(',',repars+1)+1,color.indexOf(')')))
+        r: parseInt(color.substr(5, pars)),
+        g: parseInt(color.substr(pars + 1, repars)),
+        b: parseInt(color.substr(color.indexOf(',', pars + 1) + 1, color.indexOf(',', repars))),
+        a: parseFloat(color.substr(color.indexOf(',', repars + 1) + 1, color.indexOf(')')))
       };
       /* verbous */
     } else {
       let stdCol = {
-        acqua: '#0ff', teal: '#008080', blue: '#00f', navy: '#000080',
+        aqua: '#0ff', teal: '#008080', blue: '#00f', navy: '#000080',
         yellow: '#ff0', olive: '#808000', lime: '#0f0', green: '#008000',
         fuchsia: '#f0f', purple: '#800080', red: '#f00', maroon: '#800000',
         white: '#fff', gray: '#808080', silver: '#c0c0c0', black: '#000'
@@ -177,6 +194,13 @@ export function getColorValues(color) {
     }
   }
   return values;
+}
+
+export function shadeRGBAColor(color, percent) {
+  var f = color.split(','), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent, R = parseInt(f[0].slice(5)), G = parseInt(f[1]), B = parseInt(f[2]), A = 1;
+  console.log('f', f);
+  console.info('parseInt(f[0])', parseInt(f));
+  return 'rgba('+(Math.round((t-R)*p)+R)+','+(Math.round((t-G)*p)+G)+','+(Math.round((t-B)*p)+B)+',' + A + ')';
 }
 
 function fullScreenCall() {
