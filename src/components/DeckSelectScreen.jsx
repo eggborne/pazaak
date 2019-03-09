5b7626965fec511c89a6664b1b54d867b4a8b648
Michael Donovan <mike@eggborne.com>
2019-03-08 18:19:31 -0800

    Integrate state.userStatus.wonCards

--- src/components/DeckSelectScreen.jsx
+++ src/components/DeckSelectScreen.jsx
@@ -4,13 +4,14 @@ import Card from './Card';
 
 function DeckSelectionScreen(props) {
   console.big('DeckSelectionScreen rendering');
+  console.info(props.userDeck);
   setTimeout(() => {
     document.getElementById('deck-select-screen').style.opacity = 1;
     document.getElementById('deck-select-screen').style.transform = 'none';
   }, 1);
 
-  let selectionCardSize = 'medium';
-  let selectedCardSize = 'normal';
+  let selectionCardSize = 'mini';
+  let selectedCardSize = 'mini';
   let cardsLeft = 10 - props.userDeck.length;
   let chooseStyle = {};
   let chooseText = 'Ready to play!';
@@ -28,13 +29,17 @@ function DeckSelectionScreen(props) {
       document.getElementById('deck-ready-button').classList.add('throbbing');
     });
   } else {
-    chooseText = `choose ${cardsLeft} card${cardPlural}`;
+    if (cardsLeft < 10) {
+      chooseText = `choose ${cardsLeft} more card${cardPlural}`;
+    } else {
+      chooseText = `choose ${cardsLeft} card${cardPlural}`;
+    }
     requestAnimationFrame(() => {
       document.getElementById('deck-ready-button').classList.remove('throbbing');
       document.getElementById('deck-ready-button').classList.add('disabled-button');
     });
   }
-  let cardSelectionGrid = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+  let cardSelectionGrid = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
   props.cardSelection.map((card, i) => {
     cardSelectionGrid[i] = (
       <Card
@@ -50,7 +55,13 @@ function DeckSelectionScreen(props) {
     );
   });
   let userSelectedGrid = ['', '', '', '', '', '', '', '', '', ''];
+  
+  if (!props.userDeck) {
+    props.userDeck = [];
+  }
   props.userDeck.map((card, i) => {
+    console.log('inserting')
+    console.info(card)
     userSelectedGrid[i] = (
       <Card id={card.id} context={'deck-selected'} onClickCard={props.onClickCard} size={selectedCardSize} value={card.value} type={card.type} inDeck={true} clickFunction={props.clickFunction} />
     );
@@ -63,13 +74,13 @@ function DeckSelectionScreen(props) {
   let previewCardHeight = `var(--${selectedCardSize}-card-height)`;
   let cardRadius = `calc(var(--${selectionCardSize}-card-width) / 18)`;
   let cardBorder = `calc(var(--${selectedCardSize}-card-height) / 100)`;
-
+  console.info('userSelectedGrid')
+  console.info(userSelectedGrid)
   return (
     <div id="deck-select-screen">
       <style jsx>
         {`
-          #deck-select-screen {
-            
+          #deck-select-screen {          
             font-size: calc(var(--header-height) / 2);
             font-family: var(--main-font);
             display: flex;
@@ -79,43 +90,50 @@ function DeckSelectionScreen(props) {
             align-items: center;
             opacity: 0.5;
             transform: scale(1.05);
-            //display: ${props.phase === 'selectingDeck' ? 'flex' : 'none'};
             transition: transform 300ms ease, opacity 300ms ease;
             will-change: transform, opacity;
           }
           #cards-area {
             display: flex;
             flex-direction: column;
-            justify-content: space-evenly;
+            justify-content: center;
             flex-grow: 1;
           }
-          #deck-selection-grid {
+          #deck-selection-area {
+            border: var(--inner-menu-border);
+            border-color: var(--dark-red-bg-color);
+            border-radius: var(--menu-border-radius);
+            text-align: center;
+            padding: calc(var(--menu-border-radius) * 2);
+            padding-top: 0;
+          }
+          #deck-selection-grid {                            
             display: grid;
             grid-template-columns: ${cardWidth} ${cardWidth} ${cardWidth} ${cardWidth} ${cardWidth} ${cardWidth};
-            grid-template-rows: ${cardHeight} ${cardHeight} ${cardHeight};
+            grid-template-rows: ${cardHeight} ${cardHeight} ${cardHeight} ${cardHeight};
             grid-column-gap: var(--card-buffer-size);
             grid-row-gap: var(--card-buffer-size);
           }
           #deck-select-title {
+            font-family: var(--title-font);height: calc(var(--header-height) / 1.5);
+            font-size: calc(var(--header-height) / 2);
             box-sizing: border-box;
             display: flex;
-            flex-direction: column;
             justify-content: center;
             align-items: center;
             width: 100vw;
-          }
-          #deck-select-title > div {
-            width: 95%;
-            text-align: center;
-            box-sizing: border-box;
+            height: calc(var(--header-height) / 1.25);
           }
           #preview-deck-area {
             display: flex;
             flex-direction: column;
             align-items: center;
           }
-          #preview-deck-title {
-            margin-bottom: 1.5vh;
+          #preview-deck-title, #available-deck-title {
+            height: calc(var(--menu-border-radius) * 4);
+            display: flex;
+            align-items: center;
+            justify-content: center;
           }
           #preview-deck-grid {
             box-sizing: content-box;
@@ -127,13 +145,13 @@ function DeckSelectionScreen(props) {
           }
           #preview-deck-grid > div,
           #deck-selection-grid > div {
+            border-radius: var(--menu-border-width);
             display: flex;
             justify-content: center;
             align-items: center;
             background-color: var(--card-spot-bg-color) !important;
           }
           #preview-deck-grid > div {
-            border: ${cardBorder} inset var(--card-spot-border-color);
             border-radius: calc(${cardRadius} * 2) !important;
             background-color: var(--card-spot-bg-color) !important;
           }
@@ -142,7 +160,7 @@ function DeckSelectionScreen(props) {
           }
         `}
       </style>
-      <div className="pre-header" id="deck-select-title">
+      <div id="deck-select-title">
         <div className="shadowed-text">Create deck</div>
       </div>
       <div id="choose-text" style={chooseStyle} className="smaller shadowed-text">
@@ -150,9 +168,9 @@ function DeckSelectionScreen(props) {
       </div>
       <div id="cards-area">
         <div id="deck-selection-area">
+          <div id='available-deck-title' className="smaller shadowed-text">AVAILABLE CARDS</div>
           <div id="deck-selection-grid">
             {cardSelectionGrid.map((card, i) => {
-              // return <div key={i} id={`card-selection-space-${i}`}>{card}</div>;
               return <div key={i}>{card}</div>;
             })}
           </div>
@@ -166,6 +184,7 @@ function DeckSelectionScreen(props) {
               return <div key={i}>{card}</div>;
             })}
           </div>
+        
         </div>
       </div>
     </div>
