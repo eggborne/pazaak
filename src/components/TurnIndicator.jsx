4ef56df77a9b8e38a5ac2e143549af8f9eb27503
Michael Donovan <mike@eggborne.com>
2019-03-08 18:34:15 -0800

    Add 'enter' and 'leave' animations

--- src/components/TurnIndicator.jsx
+++ src/components/TurnIndicator.jsx
@@ -12,25 +12,51 @@ function TurnIndicator(props) {
           border-radius: 50%;
           background-color: gray;
           border: 1px solid black;
-          animation-duration: 1200ms;
+          animation-duration: var(--pulse-speed);
           animation-iteration-count: infinite;
           z-index: 0;
         }
         .turn-lighted {
-          //animation-name: pulse;
+          animation-name: pulse;
           background-color: red;
+        }
+        .entering {
+          animation-name: enter;
+        }
+        .leaving {
+          animation-name: leave;
         }
         .standing {
           background-color: rgb(189, 189, 95);
         }
         @keyframes pulse {
           0%, 100% {
-            background-color: rgb(124, 0, 0);
+            opacity: 0.1;
           }
           50% {
-            background-color: rgb(255, 0, 0);
+            opacity: 1;
           }                
-        }        
+        }
+        @keyframes enter {
+          0% {
+            transform: scale(1.5);
+            opacity: 0;
+          }
+          100% {
+            transform: scale(1);
+            opacity: 1;            
+          }
+        }
+        @keyframes leave {
+          0% {
+            transform: scale(1);
+            opacity: 1;
+          }
+          100% {
+            transform: scale(1.5);
+            opacity: 0;
+          }
+        }
       `}</style>
     </div>
   );
