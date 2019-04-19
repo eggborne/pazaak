b86e605648b28e1ade14c58cc300d45ddad70044
Michael Donovan <mike@eggborne.com>
2019-04-18 22:01:39 -0700

    Add versus-mode selector

--- src/components/ModeSelectScreen.jsx
+++ src/components/ModeSelectScreen.jsx
@@ -8,11 +8,11 @@ function ModeSelectScreen(props) {
     }
   });
   let modeSelected = props.modeSelected;
+  let onlineCount = 0;
   return (
     <div id="mode-select-screen" className="shadowed-text">
       <style jsx>{`
         #mode-select-screen {
-          position: absolute;
           height: 100%;
           font-family: var(--title-font);
           line-height: 100%;
@@ -24,21 +24,58 @@ function ModeSelectScreen(props) {
           flex-direction: column;
           justify-content: space-between;
         }
-        #mode-select-choices {
+        #mode-title-area {
+          padding-top: 1vh;
+          font-size: var(--large-font-size);
+          display: grid;
+          grid-template-columns: 0.5fr 0.5fr;
+          grid-template-rows: 7vh 7vh;
+          justify-items: center;
+          align-items: center;
+          align-content: start;
+          grid-column-gap: 5vw;
+          grid-row-gap: 2vh;
+          max-height: none;
+          flex-grow: 0;
+        }
+        #mode-title {
+          grid-column-start: 0;
+          grid-column-end: span 2;
+        }
+        #lower-panel {
+          left: 0;
+          flex-grow: 1;
+          width: 200vw;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-template-rows: auto;
+        }
+        #cpu-mode-choices, #human-mode-choices {
+          width: 100vw;
           display: flex;
           flex-direction: column;
           justify-content: center;
-          align-items: center;
-          flex-grow: 1;
+          align-items: center;          
+          height: 100%;          
+          transition: transform 300ms ease;
+        }
+        #cpu-mode-choices {
+          transform: ${props.vsCPU ? 'translateX(50%)' : 'translateX(-100%)'};
+          pointer-events: ${props.vsCPU || 'none'};
+        }
+        #human-mode-choices {
+          justify-content: center;
+          transform: ${props.vsCPU ? 'translateX(100%)' : 'translateX(-50%)'};
+          pointer-events: ${props.vsCPU && 'none'};          
         }
         .mode-panel {
           box-sizing: border-box;
-          max-width: 70vmin;
+          width: var(--intro-width);
           display: flex;
           flex-direction: column;
           align-items: center;
-          justify-content: center;
-          opacity: 0.8;
+          justify-content: center;          
+          opacity: 0.75;
           transition: border 300ms ease,  opacity 300ms ease, transform 200ms ease;
           will-change: transform, opacity, border;
         }
@@ -52,8 +89,9 @@ function ModeSelectScreen(props) {
         .mode-button {
           line-height: 100%;
           box-sizing: border-box;
-          min-height: 10vh;
           padding: var(--menu-border-radius);
+          padding-top: 2vh;
+          padding-bottom: 2vh;          
           display: flex;
           flex-direction: column;
           justify-content: center;
@@ -69,10 +107,12 @@ function ModeSelectScreen(props) {
           box-sizing: border-box;
           padding: var(--menu-border-radius);
           font-family: var(--main-font);
-          width: 100%;
+          font-size: 0.75rem;
           display: flex;
+          flex-direction: column;
+          width: 50%;
         }
-        .mode-description li {
+        .mode-description > div {
           margin-top: 1vh;
           margin-bottom: 1vh;
         }
@@ -82,15 +122,40 @@ function ModeSelectScreen(props) {
         }
         .mode-panel.mode-selected-panel {    
           opacity: 1;      
-          transform: scale(1.1);
+          transform: scale(1.05);
           border: var(--menu-border-width) double var(--option-on-color) !important;
         }
-        ul {
-          position: relative;
-          list-style: square;
-          padding-inline-start: calc(var(--menu-border-width) * 4);
-          margin-block-start: 0;
-          margin-block-end: 0;
+        .versus-selector {
+          padding-left: var(--menu-border-radius);
+          padding-right: var(--menu-border-radius);
+          font-family: var(--title-font);
+          font-size: 1rem;
+          font-size: 2vh;
+          width: 65%;
+          //color: white;
+          border-radius: calc(var(--menu-border-radius) * 1.5);
+          border: calc(var(--menu-border-width) / 1.5) solid var(--dark-red-bg-color);
+          background-color: #44444444;     
+          line-height: 120%;     
+          align-self: stretch;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          transition: background-color 210ms ease, border-color 210ms ease;
+        }
+        #ai-selector {
+          justify-self: end;
+        }
+        #humans-selector {
+          justify-self: start;          
+        }
+        #online-indicator {
+          font-family: var(--main-font);
+          font-size: 0.75em;
+        }
+        .versus-selector.selected {
+          background-color: #00ff0066;
+          border-color: #00ff00dd;
         }
         .progress-message {
           font-family: var(--main-font);
@@ -102,48 +167,64 @@ function ModeSelectScreen(props) {
           margin-top: var(--menu-border-width);
         }
       `}</style>
-      <div className="options-instructions-title">Game Mode</div>
-      <div id="mode-select-choices">
-        {/* <div  {...{ [props.clickFunction]: props.onClickCampaign }} className={'mode-panel' + (modeSelected === 'campaign') && ' mode-selected-panel' }> */}
-        <div {...{ [props.clickFunction]: props.onClickCampaign }} className={`mode-panel red-panel ${modeSelected === 'campaign' && 'mode-selected-panel'}`}>
-          <div className='inner-red-panel'>
-            <div className={`mode-button ${modeSelected === 'campaign' && 'mode-selected-button'}`} id="campaign-button">
-              Campaign
-              <div className='progress-message'>{props.cpuDefeated} opponent{props.cpuDefeated !== 1 && 's'} defeated</div>
-              <div className='progress-message'>{props.cardsWon} card{props.cardsWon !== 1 && 's'} won</div>
+      <div id='mode-title-area' className="options-instructions-title">
+        <div id='mode-title'>Game Mode</div>
+        <div {...{ [props.clickFunction]: event => props.onClickSwitchVersusMode(event, 'vsCPU') } } id='ai-selector' className={props.vsCPU ? 'versus-selector selected' : 'versus-selector'}>vs. COMPUTER</div>
+        <div {...{ [props.clickFunction]: event => props.onClickSwitchVersusMode(event, 'vsHumans') } } id='humans-selector' className={props.vsCPU ? 'versus-selector' : 'versus-selector selected'}>
+          <div>vs. HUMANS</div>
+          <div id='online-indicator'>{onlineCount} online</div>
+        </div>
+      </div>
+
+      <div id='lower-panel'>
+
+        <div id="cpu-mode-choices">
+          {/* <div  {...{ [props.clickFunction]: props.onClickCampaign }} className={'mode-panel' + (modeSelected === 'campaign') && ' mode-selected-panel' }> */}
+          <div {...{ [props.clickFunction]: props.onClickCampaign }} className={`mode-panel red-panel ${modeSelected === 'campaign' && 'mode-selected-panel'}`}>
+            <div>
+              <div className={`mode-button ${modeSelected === 'campaign' && 'mode-selected-button'}`} id="campaign-button">
+                Campaign
+                <div className='progress-message'>{props.cpuDefeated} opponent{props.cpuDefeated !== 1 && 's'} defeated</div>
+                <div className='progress-message'>{props.cardsWon} card{props.cardsWon !== 1 && 's'} won</div>
+              </div>
             </div>
             <div className="mode-description">
-              <ul>
-                <li>Climb a roster of crafty AI opponents</li>
-                <li>Win credits / special cards to progress</li>
-              </ul>
+              <div>&middot; Climb a roster of crafty AI opponents</div>
+              <div>&middot; Win credits / special cards to progress</div>
             </div>
           </div>
-        </div>
-        <div {...{ [props.clickFunction]: props.onClickQuickMatch }} className={`mode-panel red-panel ${modeSelected === 'quick' && 'mode-selected-panel'}`}>
-          <div className='inner-red-panel'>
-            <div className={`mode-button ${modeSelected === 'quick' && 'mode-selected-button'}`} id="quick-match-button">
-              Quick Match
+          <div {...{ [props.clickFunction]: props.onClickQuickMatch }} className={`mode-panel red-panel ${modeSelected === 'quick' && 'mode-selected-panel'}`}>
+            <div>
+              <div className={`mode-button ${modeSelected === 'quick' && 'mode-selected-button'}`} id="quick-match-button">
+                Quick Match
+              </div>
             </div>
             <div className="mode-description">
-              <ul>
-                <li>Single match with a random opponent</li>
-                <li>Wager up to 2000 credits</li>
-              </ul>
+              <div>&middot; Single match with a random AI opponent</div>
+              <div>&middot; Wager any amount of credits</div>
             </div>
-          </div>
+          </div>            
         </div>
+
+        <div id='human-mode-choices'>
+          <div>
+            unavailable :(
+          </div>
+        </div>        
+        
       </div>
     </div>
   );
 }
 ModeSelectScreen.propTypes = {
   phase: PropTypes.string,
+  vsCPU: PropTypes.bool,
   cpuDefeated: PropTypes.number,
   cardsWon: PropTypes.number,
   modeSelected: PropTypes.string,
   onClickCampaign: PropTypes.func,
   onClickQuickMatch: PropTypes.func,
+  onClickSwitchVersusMode: PropTypes.func,
   clickFunction: PropTypes.string
 };
 
